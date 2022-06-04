0f3ee9c022ddc5d477e8ee64692fed9c9f457750
ampharino <ampharin@gmail.com>
2022-06-04 09:43:00 +0000

    Deploying to gh-pages from @ ampharino/ampharino.github.io@45a20cc27532210e6ba6fb98131943fdb2bce503 🚀

---
+++ _next/static/A_pgzI4KL-XawHUSqiXON/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c06aab4da43056f2.css","static/chunks/pages/index-340e2122dade4930.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
