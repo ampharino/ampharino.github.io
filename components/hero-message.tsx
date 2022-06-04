98c7affaf4e1512c097a1fa64b5dd0646bc8905d
Amp <ampharin@gmail.com>
2022-06-04 16:16:15 +0700

    Redo styling for hero section

--- components/hero-message.tsx
+++ components/hero-message.tsx
@@ -4,15 +4,12 @@ const HeroMessage = () => {
   return (
     <div className={styles["hero-message-container"]}>
       <div className={styles["hero-message"]}>
-        <h1 className={styles.typewriter}>
-          {"Hello! I'm Amp "}
-          <span className={styles["wave-emoji"]}>👋 </span>
-        </h1>
+        <h1 className={styles.typewriter}>{"Ampharin Ongvises "}</h1>
       </div>
-      <div className={styles["hero-content"]}>
-        {
-          "I'm a Software Developer currently based in Bangkok. I'm somewhat of a specialist in being a generalist, having worked with a wide variety of tools and technologies such as React / Vue / Javascript / Typescript / Java / Kotlin / Python / C#. React is probably my favorite though."
-        }
+      <div className={styles["hero-subtitle"]}>
+        <h3 className={styles["typewriter--subtitle"]}>
+          {"Software Engineer in Bangkok"}
+        </h3>
       </div>
     </div>
   )
